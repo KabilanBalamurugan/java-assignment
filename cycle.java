c3e735a4d891e31055fab678150e8774cac643a2
Kabilan <129851318+KabilanBalamurugan@users.noreply.github.com>
2024-05-08 23:32:10 -0400

    cycle.java

--- cycle.java
+++ cycle.java
@@ -1,18 +1,18 @@
 class BiCycle{
-	String define_me(){
+	String name(){
 		return "a vehicle with pedals.";
 	}
 }
 
 class MotorCycle extends BiCycle{
-	String define_me(){
+	String name(){
 		return "a cycle with an engine.";
 	}
 	
 	MotorCycle(){
 		System.out.println("Hello I am a motorcycle, I am "+ define_me());
      
-		String temp=define_me(); //Fix this line
+		String temp=name();
 
 		System.out.println("My ancestor is a cycle who is "+ temp );
 	}
