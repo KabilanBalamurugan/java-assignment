7f7d194d88d99c09cef3c67a06920061f257d71f
Kabilan <129851318+KabilanBalamurugan@users.noreply.github.com>
2024-05-12 23:41:31 -0400

    Update demofor.java

--- demofor.java
+++ demofor.java
@@ -2,14 +2,14 @@ public class demofor {
     public static void main(String[] args) {
         int n=5;
         for(int i=0;i<n;i++){
-            // for(int j=0;j<(n-i);j++){
-            //     System.out.print(j+1+" ");
+            for(int j=0;j<(n-i);j++){
+                System.out.print(j+1+" ");
                 
-            //  }
-            //  for (int k=0;k<=n;k++)
-            //  {
-            //     System.out.print(k+"");
-            //  }
+             }
+             for (int k=0;k<=n;k++)
+             {
+                System.out.print(k+"");
+             }
              System.out.println();
             if(i>=5)
             {
