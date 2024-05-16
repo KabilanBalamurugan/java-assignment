3adde7d7a0d85bd2c49d4bf6251af7ae77a38e0e
Kabilan <129851318+KabilanBalamurugan@users.noreply.github.com>
2024-05-15 23:58:38 -0400

    Create demopackage.java

---
+++ demopackage.java
@@ -0,0 +1,15 @@
+
+class demopack extends studentdata {
+    demopack(String city,String st_name,int plotno,int pincode ){
+        super(city,st_name,plotno,pincode);
+        
+        super.displayaddress();
+        
+    }
+}
+public class demopackage {
+    public static void main(String[] args) {
+      new demopack( "Madurai","LPN Street", 1, 625007);
+       
+    }
+}
