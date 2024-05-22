a9f36a2f81b367c7686cbfa29d0a07e0c1c9f17d
Kabilan <129851318+KabilanBalamurugan@users.noreply.github.com>
2024-05-22 00:17:02 -0400

    demoif.js

---
+++ demoif.js
@@ -0,0 +1,13 @@
+let age=32;
+
+if(age>=18)
+    {
+        console.log("You're eligble")
+    }else
+    {
+        console.log("Wait for next election")
+    }
+// ternary operator
+
+let result=age>=18 ? "You're eligible for voting":"Wait for the turn"   //If condition true => executes statement after ?
+console.log(result);                                                   //If condition false=> executes statement after :
