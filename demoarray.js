8e70c1062b5dcdb2479af762c868f27f0eddcc38
Kabilan <129851318+KabilanBalamurugan@users.noreply.github.com>
2024-05-26 23:42:40 -0400

    demoarray.js

---
+++ demoarray.js
@@ -0,0 +1,20 @@
+const x=[1,2,3]
+const y=[4,5,6]
+x[0]=18
+
+x[1]=15
+
+// console.log(z);
+y[0]=21
+const z=x.concat(y)
+z[1]=10;
+console.log(z)
+console.log(y)
+console.log(x)
+
+
+var state=[]
+state.push("TN","KA","AP",'KL');
+console.log(state)
+state.copyWithin(1,2);
+console.log(state);
