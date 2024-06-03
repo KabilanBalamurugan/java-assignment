9fa804351af4bb8f4c7d748c6fdc5b65edceb3d9
Kabilan <129851318+KabilanBalamurugan@users.noreply.github.com>
2024-06-03 00:09:30 -0400

    Asynchronous Function

---
+++ Day21/asynchronous.js
@@ -0,0 +1,15 @@
+//Normal Function
+function callme() {
+    console.log("Call Me");
+    return 1;
+}
+console.log(callme());
+callme();
+
+//Async Function
+
+async function callMe() 
+{
+    return "Hello";    
+}
+console.log(callMe());
