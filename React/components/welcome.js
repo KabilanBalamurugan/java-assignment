8c03ed773b6e8b11e12313b26ac137664885e689
Kabilan <129851318+KabilanBalamurugan@users.noreply.github.com>
2024-06-03 23:55:27 -0400

    Create welcome.js

---
+++ React/components/welcome.js
@@ -0,0 +1,11 @@
+import React from 'react'
+function Welcome(){
+    return(
+        <>
+        <h1>Welcome to React</h1>
+        <p>Hello</p>
+        </>
+        
+    )
+}
+export default Welcome;
