d8cde151e97e7b799ee2b6dbb1d94ea6f05ab760
Kabilan <129851318+KabilanBalamurugan@users.noreply.github.com>
2024-06-03 23:54:58 -0400

    Create paragraph.js

---
+++ React/components/paragraph.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+// function Paragraph() {
+//     return(
+//         <p>Components</p>
+//     )
+// }
+// export default Paragraph;
+
+// export default function Paragraph()
+// {
+//     return(
+//         <h1>Paragraph</h1>
+//     )
+// }
+
+//  let Paragraph=(props)=>{return(<h1>Paragraph1{props.name}</h1>)}                        //Arrow Function
+
+
+
+let Paragraph=(name)=>{return(<h1>Paragraph1{name}</h1>)} 
+export default Paragraph;
